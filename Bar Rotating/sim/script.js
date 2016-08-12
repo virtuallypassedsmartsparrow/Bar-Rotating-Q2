2c69f55993d8f174109c304e3d3d468a936ef37c
virtuallypassedsmartsparrow <virtuallypassed@gmail.com>
2016-08-12 12:46:03 +1200

    Update script.js

--- Bar Rotating/sim/script.js
+++ Bar Rotating/sim/script.js
@@ -330,7 +330,7 @@ function getShowVariables() {
                     break;
                 case "60":
                 case "70":
-                    return ["w_2"];
+                    return ["w_2", "V_2"];
                     break;
                 case "80":
                     return ["w_2", "V_2", "alpha_2", "O_x_2"];
